ca019b5d16c39071cea7c21975709a67b2a09509
sammax12 <vitebskiycollege@gmail.com>
2022-05-04 20:41:02 -0700

    Set UP React and Router?

---
+++ Temperature-Web/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Home from './components/Home';
+
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App;
